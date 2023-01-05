b683fbcc6eb143947edaf238f63991acbad3a92e
Nick Lalic <nick.lalic@gmail.com>
2023-01-05 12:08:31 -0700

    🔨 build boost log only; do not build unnecessary boost modules

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,41 @@
-from conans import ConanFile, CMake
+from conans import CMake, ConanFile
 
 
 class BoostLog(ConanFile):
-   settings = "os", "compiler", "build_type", "arch"
-   requires = "boost/1.80.0"
-   generators = "cmake"
+    name       = 'BoostLog'
+    version    = '1.0.0'
+    settings   = 'os', 'compiler', 'build_type', 'arch'
+    requires   = 'boost/1.80.0'
+    generators = 'cmake'
 
-   def imports(self):
-      self.copy("*.dll",    dst="bin", src="bin") # From bin to bin
-      self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
-      self.copy("*.so*",    dst="bin", src="lib")
+    def configure(self):
+        self.options['boost'].without_context         = True
+        self.options['boost'].without_contract        = True
+        self.options['boost'].without_coroutine       = True
+        self.options['boost'].without_fiber           = True
+        self.options['boost'].without_graph           = True
+        self.options['boost'].without_graph_parallel  = True
+        self.options['boost'].without_iostreams       = True
+        self.options['boost'].without_json            = True
+        self.options['boost'].without_locale          = True
+        self.options['boost'].without_math            = True
+        self.options['boost'].without_mpi             = True
+        self.options['boost'].without_nowide          = True
+        self.options['boost'].without_program_options = True
+        self.options['boost'].without_python          = True
+        self.options['boost'].without_serialization   = True
+        self.options['boost'].without_stacktrace      = True
+        self.options['boost'].without_test            = True
+        self.options['boost'].without_timer           = True
+        self.options['boost'].without_type_erasure    = True
+        self.options['boost'].without_wave            = True
 
-   def build(self):
+    def build(self):
       cmake = CMake(self)
       cmake.configure()
       cmake.build()
+
+    def imports(self):
+        self.copy('*.dll',   'bin', 'bin')
+        self.copy('*.dylib', 'bin', 'lib')
+        self.copy('*.so',    'bin', 'lib')
